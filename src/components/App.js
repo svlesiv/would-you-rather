c180f18e47f8d465e0b441c490ec026d9a1ec80b
Svitlana Lesiv <lesivsvitlana@gmail.com>
2019-01-07 18:22:37 -0800

    feat: Add navigation

--- src/components/App.js
+++ src/components/App.js
@@ -3,7 +3,11 @@ import { connect } from 'react-redux'
 import { BrowserRouter as Router, Route } from 'react-router-dom'
 import { handleInitialData } from '../actions/shared'
 
+// import PrivateRoute from './PrivateRoute'
+import Nav from './Nav'
+import Login from './Login'
 import HomePage from './HomePage'
+import NewQuestion from './NewQuestion'
 import QuestionPage from './QuestionPage'
 
 class App extends Component {
@@ -17,8 +21,13 @@ class App extends Component {
     return (
       <Router>
         <div>
-          <Route path='/' exact component={HomePage} />
-          <Route path='/question/:id' component={QuestionPage} />
+          <Nav />
+          <div>
+            <Route path='/login' exact component={Login} />
+            <Route path='/' exact component={HomePage}/>
+            <Route path='/new' exact component={NewQuestion}/>
+            <Route path='/question/:id' exact component={QuestionPage}/>
+          </div>
         </div>
       </Router>
     );
