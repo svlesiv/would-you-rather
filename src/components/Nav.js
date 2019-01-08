16866eaa24bb7f44afc1b3cc5a75e90cf01604c5
Svitlana Lesiv <lesivsvitlana@gmail.com>
2019-01-08 11:42:13 -0800

    feat: Toggle logout btn visibility

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,26 +1,44 @@
-import React from 'react'
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
 import { NavLink } from 'react-router-dom'
+import { logOut } from '../actions/authedUser'
 
-export default function Nav () {
-  return (
-    <nav>
-      <ul>
-        <li>
-          <NavLink to='/' exact activeClassName='active'>
-            Home
-          </NavLink>
-        </li>
-        <li>
-          <NavLink to='/new' activeClassName='active'>
-            New Question
-          </NavLink>
-        </li>
-        <li>
-          <NavLink to='/leaders' activeClassName='active'>
-            Leader Board
-          </NavLink>
-        </li>
-      </ul>
-    </nav>
-  )
+class Nav extends Component {
+  render(){
+    return (
+      <nav>
+        <ul>
+          <li>
+            <NavLink to='/' exact activeClassName='active'>
+              Home
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to='/new' activeClassName='active'>
+              New Question
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to='/leaders' activeClassName='active'>
+              Leader Board
+            </NavLink>
+          </li>
+          <li>
+            {this.props.isAuthenticated
+              ? <button>Logout</button>
+              : null
+            }
+          </li>
+        </ul>
+      </nav>
+    )
+  }
 }
+
+function mapStateToProps ({users, authedUser}) {
+  return {
+    isAuthenticated: users[authedUser],
+  }
+}
+
+export default connect(mapStateToProps)(Nav)
