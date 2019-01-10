a2a9fefe586a127da0a73ebf3eca61740583129d
Svitlana Lesiv <lesivsvitlana@gmail.com>
2019-01-09 17:20:18 -0800

    feat: Redirect after submission

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,8 +1,26 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
-import { formatQuestion, formatDate } from '../utils/helpers'
+import { formatQuestion } from '../utils/helpers'
+import { withRouter } from 'react-router-dom'
 
 class Question extends Component {
+  state = {
+    option: 0,
+  }
+
+  handleChange = (num) => {
+    this.setState({
+      option: num
+    })
+  }
+
+  handleSubmit = (e) => {
+    e.preventDefault()
+
+    //redirect after submission
+    this.props.history.push(`/statistic/${this.props.id}`)
+  }
+
   render() {
     const { question, user } = this.props
     const { author, optionOne, optionTwo } = question
@@ -11,19 +29,18 @@ class Question extends Component {
       <div>
         <p>{author} asks:</p>
         <img src={user.avatarURL} />
-        <form>
+        <form onSubmit={this.handleSubmit}>
           <h1>Would You Rather...</h1>
 
-          <input type="radio" name="question" value="optionOne" defaultChecked />
+          <input type="radio" name="question" value="optionOne" defaultChecked onChange={()=>this.handleChange(0)}/>
           <label htmlFor="optionOne">{optionOne.text}</label>
 
-          <input type="radio" name="question" value="optionTwo" />
+          <input type="radio" name="question" value="optionTwo" onChange={()=>this.handleChange(1)}/>
           <label htmlFor="optionTwo">{optionTwo.text}</label>
 
-          <button
-              type='submit'>
+          <button type='submit'>
                 Submit
-            </button>
+          </button>
         </form>
       </div>
     )
@@ -32,7 +49,7 @@ class Question extends Component {
 
 function mapStateToProps ({users, questions}, { id }) {
   const question = questions[id]
-   return {
+  return {
     user: users[question.author],
     question: question
       ? formatQuestion(question)
@@ -40,4 +57,4 @@ function mapStateToProps ({users, questions}, { id }) {
   }
 }
 
-export default connect(mapStateToProps)(Question)
+export default withRouter(connect(mapStateToProps)(Question))
