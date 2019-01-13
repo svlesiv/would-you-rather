9d5ccde7c0d661553c6ba59d7e6979c67007d53a
Svitlana Lesiv <lesivsvitlana@gmail.com>
2019-01-12 16:53:33 -0800

    feat: Sort Leaderboard

--- src/components/LeaderBoard.js
+++ src/components/LeaderBoard.js
@@ -3,26 +3,54 @@ import { connect } from 'react-redux'
 
 class LeaderBoard extends Component {
   render() {
-    const { users, usersIds } = this.props
+    const { users, usersIds,sum, sortedIds } = this.props
     return (
       <div>
-        {usersIds.map((user)=>(
+        {sortedIds.map((user)=>(
           <div key={user}>
             <p>{users[user].name}</p>
             <img src={users[user].avatarURL}/>
             <p>Answered questions: {Object.keys(users[user].answers).length}</p>
             <p>Created questions: {users[user].questions.length}</p>
           </div>
-        ))}
+        )) }
       </div>
     )
   }
 }
 
 function mapStateToProps ({users}) {
+   //maybe better to have array with two values, something like tuples...
+
+   //https://stackoverflow.com/questions/37982476/how-to-sort-a-map-by-value-in-javascript
+   //sort first alphabetically and then by values
+   function sortMapByValue(myMap) {
+     myMap[Symbol.iterator] = function* () {
+       yield* [...this.entries()].sort().sort((a, b) => b[1] - a[1]);
+     }
+   }
+
+   function storeKeys (myArray, myMap) {
+     for (let [key, value] of myMap) {
+       myArray.push(key)
+     }
+   }
+
+   let sumMap = new Map()
+   let sortedIds = []
+
+   //populate map
+   Object.keys(users).map((user)=>{
+     sumMap.set(user, (Object.keys(users[user].answers).length)
+                      + (Object.keys(users[user].questions).length))
+   })
+
+   sortMapByValue(sumMap)
+   storeKeys(sortedIds, sumMap)
+
    return {
-    usersIds: Object.keys(users),
-    users,
+     users,
+     sortedIds
   }
 }
 
