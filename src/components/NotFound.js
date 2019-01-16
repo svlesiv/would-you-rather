177b2d84a80dc2ff7a6e1b03aa33c6ded65f89d4
Svitlana Lesiv <lesivsvitlana@gmail.com>
2019-01-16 13:55:22 -0800

    style: Change markup and style for 404 page

--- src/components/NotFound.js
+++ src/components/NotFound.js
@@ -3,8 +3,9 @@ import React, { Component } from 'react';
 class NotFound extends Component {
   render() {
     return (
-      <div>
-        <p>404 page</p>
+      <div className='not-found-container'>
+        <h1>404</h1>
+        <h2>Sorry, page not found :(</h2>
       </div>
     )
   }
